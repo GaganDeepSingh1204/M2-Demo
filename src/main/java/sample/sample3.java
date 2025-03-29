3b039a5b2c8edb0925c581c12a5f7851f8e6ba7c
gd377 <gd377@DEEP.cctv>
2025-03-29 09:32:02 +0530

    3rd Changes

--- src/main/java/sample/sample3.java
+++ src/main/java/sample/sample3.java
@@ -2,10 +2,10 @@ package sample;
 
 import org.testng.annotations.Test;
 
-public class sample2 {
+public class sample3{
 @Test
 
- public void demo2() {
+ public void demo3() {
 	System.out.println("Sample 2");
 }
 }
